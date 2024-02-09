cb812f3cb79c8bc3fee7eb3c502665ac4194582e
piotr-lewandowski <lewy007@o2.pl>
2024-02-09 23:09:10 +0100

    3rd commit - Updated test

--- src/test/java/pl/lewandowski/jenkinsspringexample/JenkinsSpringExampleApplicationTests.java
+++ src/test/java/pl/lewandowski/jenkinsspringexample/JenkinsSpringExampleApplicationTests.java
@@ -1,6 +1,7 @@
 package pl.lewandowski.jenkinsspringexample;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @AutoConfigureMockMvc
 class JenkinsSpringExampleApplicationTests {
 
+    @Autowired
     private MockMvc mockMvc;
 
     @Test
