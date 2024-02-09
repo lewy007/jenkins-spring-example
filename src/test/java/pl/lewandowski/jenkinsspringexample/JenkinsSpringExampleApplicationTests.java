4f37ef40afd845f9953857141a9976ef17927a30
piotr-lewandowski <lewy007@o2.pl>
2024-02-09 16:00:05 +0100

    Initial commit

---
+++ src/test/java/pl/lewandowski/jenkinsspringexample/JenkinsSpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lewandowski.jenkinsspringexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
